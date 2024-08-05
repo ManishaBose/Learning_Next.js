31e72e9204bfcd0542e982d538a9f76c96ea0882
MANISHA BOSE <manishabose.2001@gmail.com>
2024-08-05 20:17:14 +0530

    Improve routing

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -1,4 +1,27 @@
-export default function Signin() {
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  type?: string;
+}
+
+function LabelledInput({ label, placeholder, type }: LabelledInputType) {
+  return (
+    <div>
+      <label className="block mb-2 text-sm text-black font-semibold pt-4">
+        {label}
+      </label>
+      <input
+        type={type || "text"}
+        id="first_name"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
+
+export function SigninComponent() {
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -21,6 +44,9 @@ export default function Signin() {
                 placeholder="123456"
               />
               <button
+                onClick={function () {
+                  console.log("I am tired of Hi there");
+                }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
@@ -30,29 +56,6 @@ export default function Signin() {
           </div>
         </a>
       </div>
-    </div>
-  );
-}
-
-interface LabelledInputType {
-  label: string;
-  placeholder: string;
-  type?: string;
-}
-
-function LabelledInput({ label, placeholder, type }: LabelledInputType) {
-  return (
-    <div>
-      <label className="block mb-2 text-sm text-black font-semibold pt-4">
-        {label}
-      </label>
-      <input
-        type={type || "text"}
-        id="first_name"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        placeholder={placeholder}
-        required
-      />
     </div>
   );
 }
